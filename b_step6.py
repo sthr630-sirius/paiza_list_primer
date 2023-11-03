9e934ead7b78eb036795e16014c2dda172172dfa
sthr630-sirius <s.izumi630g@gmail.com>
2023-11-04 07:02:03 +0900

    edit b_step6

---
+++ b_step6.py
@@ -0,0 +1,40 @@
+n, p = map(int, input().split())
+size_array = 1024
+start_idx = 0
+end_idx = 1023
+value = [None]*size_array
+next_ptr = [None]*size_array
+prev_ptr = [None]*size_array
+value[start_idx] = -1
+value[end_idx] = -1
+next_ptr[start_idx] = 1023
+next_ptr[end_idx] = -1
+prev_ptr[start_idx] = -1
+prev_ptr[end_idx] = 0
+empty_min_idx = 1
+
+for _ in range(n):
+    a = int(input())
+    value[empty_min_idx] = a
+    next_ptr[empty_min_idx] = end_idx
+    next_ptr[prev_ptr[end_idx]] = empty_min_idx
+    prev_ptr[empty_min_idx] = prev_ptr[end_idx]
+    prev_ptr[end_idx] = empty_min_idx
+    empty_min_idx += 1
+
+target_id = 0
+idx = next_ptr[start_idx]
+counter = 1
+while counter <= p:
+    if idx == p:
+        target_id = idx
+    counter += 1
+    idx = next_ptr[idx]
+
+next_ptr[prev_ptr[target_id]] = next_ptr[target_id]
+prev_ptr[next_ptr[target_id]] = prev_ptr[target_id]
+
+print_idx = next_ptr[start_idx]
+while print_idx != end_idx:
+    print(value[print_idx])
+    print_idx = next_ptr[print_idx]
